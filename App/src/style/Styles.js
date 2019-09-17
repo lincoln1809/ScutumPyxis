9aa3d36930342b5aaf5c831d172d94012aa976df
Filipe <filipe.tramujas@gmail.com>
2019-09-17 17:53:15 -0300

    cria tela login

--- App/src/style/Styles.js
+++ App/src/style/Styles.js
@@ -38,8 +38,50 @@ export default StyleSheet.create({
 		justifyContent: 'space-around'
 	},
 	helpButtonText: {
-		fontSize: 12,
+		fontSize: 14,
 		color: '#005F80',
+	},
+
+	//Enter Button
+
+	enterButtonPosition: {
+		alignItems: 'center',
+	},
+	enterButtonContent: {
+		borderRadius: 5,
+		borderWidth: 2,
+		borderColor: '#008E51',
+		width: 160,
+		height: 45,
+		alignItems: 'center',
+		justifyContent: 'space-around'
+	},
+	enterButtonText: {
+		fontWeight: 'bold',
+		textTransform: 'uppercase',
+		fontSize: 14,
+		color: '#008E51',
+	},
+
+	//Cancel Button EA3D2F
+
+	cancelButtonPosition: {
+		alignItems: 'center',
+	},
+	cancelButtonContent: {
+		borderRadius: 5,
+		borderWidth: 2,
+		borderColor: '#EA3D2F',
+		width: 160,
+		height: 45,
+		alignItems: 'center',
+		justifyContent: 'space-around'
+	},
+	cancelButtonText: {
+		textTransform: 'uppercase',
+		fontWeight: 'bold',
+		fontSize: 14,
+		color: '#EA3D2F',
 	},
 
 	//Image
@@ -49,6 +91,15 @@ export default StyleSheet.create({
 		height: 200,
 	},
 	imagePosition: {
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	imageSizeLogin: {
+		width: 131,
+		height: 122,
+	},
+	imagePositionLogin: {
 		justifyContent: 'space-around',
 		alignItems: 'center',
 		justifyContent: 'center',
@@ -70,5 +121,37 @@ export default StyleSheet.create({
 		flex: 2,
 		alignItems: 'center',
 		justifyContent: 'space-around',
+	},
+	centerReverse: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
+	bottomReverse: {
+		flex: 2,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+
+	},
+	topReverse: {
+		flex: 2,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
+
+	//Input
+
+	input: {
+		flex: 4,
+		paddingLeft: 10,
+		paddingRight: 10,
+		paddingBottom: 20,
+		flexDirection: 'column',
+	},
+	inputField: {
+		color: '#005F80',
+		paddingBottom: 50,
 	}
 })
--- App/src/views/Login.js
+++ App/src/views/Login.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import Styles from '../style/Styles'
+import { Button } from "../components/button"
+import { TextInput } from 'react-native-paper';
 
+const theme = {
+    roundness: 5,
+    colors: {
+      primary: '#005F80',
+      accent: '#f1c40f',
+    }
+  };
 export default class Login extends Component {
     render() {
+        const { navigate } = this.props.navigation;
         return (
-            <View style={ Styles.container }>
-                <Text>Essa é a tela de login</Text>
-            </View>
+            <View style={Styles.container}>
+				<View style={Styles.top}>
+					<Image style={Styles.imageSizeLogin}
+						source={require('../img/splogo.png')}
+						resizeMode="contain" />
+				</View>
+				<View style={Styles.input}>
+                    <View style={Styles.inputField}>
+                        <TextInput
+                            theme={theme}
+                            style={Styles.color}
+                            label='Email/CPF'
+                            mode='outlined'
+                        />
+                    </View>
+                    <View style={ Styles.inputField }>
+                        <TextInput
+                            theme={theme}
+                            label='Senha'
+                            mode='outlined'
+                        />
+                    </View>
+                    <View style={ Styles.bottomReverse }>
+                        <Button
+                        style={ Styles.cancelButtonContent }
+						textStyle={ Styles.cancelButtonText }
+						title="Cancelar"
+						onPress={() => navigate('Login')}/>
+                        <Button
+                        style={ Styles.enterButtonContent }
+						textStyle={ Styles.enterButtonText }
+						title="Entrar"
+						onPress={() => navigate('Login')}/>
+                    </View>
+                    <View style = {Styles.bottom}>
+					<Button
+						style={ Styles.helpButtonContent }
+						textStyle={ Styles.helpButtonText }
+						title="Esqueci minha senha"
+						onPress={() => navigate('Logon')}
+					/>
+				</View>
+                </View>
+			</View>
         )
     }
 }
