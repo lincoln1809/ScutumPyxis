8015c5c9d4bd3247ab13dc00e9fa5afd8083c9c5
lincoln1809 <filipe.calabrez@gmail.com>
2019-10-15 10:18:24 -0300

    Revert "Feature/cria sistema de autenticação do usuario e backend"

--- App/App.js
+++ App/App.js
@@ -1,49 +1,114 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator} from 'react-navigation-stack'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-//screens import
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-import MainScreen from './src/views/MainScreen'
-import Logon from './src/views/Logon'
-import Login from './src/views/Login'
-import UserScreen from './src/views/UserScreen'
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-const App = createStackNavigator({
-    Home: {
-      screen: MainScreen,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    Login: {
-      screen:  Login,
-        navigationOptions: {
-          header: null,
-      },
-    },
-    Logon: {
-      screen: Logon,
-      navigationOptions: {
-        title: 'Crie sua conta',
-        headerStyle: {
-          backgroundColor: '#005F80',
-        },
-        headerTintColor: '#fefefe',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-    },
-    },
-    UserScreen: {
-      screen: UserScreen,
-      navigationOptions: {
-        header: null,
-        gesturesEnabled: false,
-      },
-    },
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  {
-    initialRouteName: 'Home',
-  }
-);
-export default createAppContainer(App);
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
